ed214a1f29c3e7367c9f0f3310b23fe9ae46cd72
mohamedhany01 <mohamedhyouns@gmail.com>
2021-11-13 20:20:33 +0200

    Add SECRET_KEY env var

--- finance/__init__.py
+++ finance/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 """App configrations"""
 app.config["TEMPLATES_AUTO_RELOAD"]             = True                      # Ensure templates are auto-reloaded
-app.config["SECRET_KEY"]                        = os.urandom(32)            # Add a secret key to enable flashed messages and sessions
+app.config["SECRET_KEY"]                        = os.environ.get("SECRET_KEY")        # Add a secret key to enable flashed messages and sessions
 app.config["SQLALCHEMY_DATABASE_URI"]           = "sqlite:///finance.db"    # A relative path of sqlite db
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]    = False
 database = SQLAlchemy(app)
